5e2cb9c8a000908099b7bc9ea7e6e049ddbdcc3c
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2018-10-30 19:59:22 +0530

    Finish problem 3

---
+++ Lab-4/3/parent-child.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main() {
+  int pipefd[2];
+  pid_t cpid;
+  char buffer[1024];
+  FILE *p = fopen("input", "r");
+  FILE *q = fopen("output", "w");
+  char string[1024];
+  fscanf(p, "%s", string);
+  // Create the IPC channel
+  pipe(pipefd);
+  cpid = fork();
+  if (cpid == 0) {
+    close(pipefd[1]);
+    while (read(pipefd[0], buffer, 1024) > 0)
+      ;
+    fprintf(q, "%s", buffer);
+    close(pipefd[0]);
+  } else {
+    close(pipefd[0]);
+    write(pipefd[1], string, strlen(string));
+    close(pipefd[1]);
+    wait(NULL);
+  }
+  return 0;
+}
