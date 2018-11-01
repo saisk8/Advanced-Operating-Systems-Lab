43be9c3d1291bb07c2c63a7504c3f53ea4861826
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2018-11-01 13:16:58 +0530

    Remove the allocation list
    Might have semantic errors.

--- Lab-5/d.c
+++ Lab-5/d.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 void firstFit() {
-  int block_arr[10], file_arr[10], allocated[10] = {0};
+  int block_arr[10], file_arr[10];
   int fragments[10] = {0}, blocks[10], files[10];
   int m, n, number_of_blocks, number_of_files, temp;
-  int externalFragmentation = 0;
   printf("\nEnter the Total Number of Blocks:\t");
   scanf("%d", &number_of_blocks);
   printf("Enter the Total Number of Files:\t");
@@ -25,33 +24,24 @@ void firstFit() {
         temp = blocks[n] - files[m];
         if (temp >= 0) {
           file_arr[m] = n;
-          allocated[m] = 1;
           break;
         }
       }
-    }
-    if (!allocated[m]) {
-      printf("No block can be allocated for file %d\n", m + 1);
-      continue;
     }
     fragments[m] = temp;
     block_arr[file_arr[m]] = 1;
   }
   printf("\nFile Number\tBlock Number\tFile Size\tBlock Size\tFragment");
   for (m = 0; m < number_of_files; m++) {
-    externalFragmentation += fragments[m];
-    if (allocated[m])
       printf("\n%d\t\t%d\t\t%d\t\t%d\t\t%d", m, file_arr[m], files[m],
              blocks[file_arr[m]], fragments[m]);
   }
-  printf("\nThe external fragmentation is: %d\n\n", externalFragmentation);
 }
 
 void bestFit() {
   int fragments[10] = {0}, block[10], file[10], m, n, number_of_blocks,
-      number_of_files, temp, lowest = 10000, allocated[10] = {0};
+      number_of_files, temp, lowest = 10000;
   static int block_arr[10], file_arr[10];
-  int externalFragmentation;
   printf("\nEnter the Total Number of Blocks:\t");
   scanf("%d", &number_of_blocks);
   printf("\nEnter the Total Number of Files:\t");
@@ -84,22 +74,15 @@ void bestFit() {
       }
       lowest = 10000;
     }
-    if (!allocated[m] && n == number_of_blocks - 1) {
-      printf("No block can be allocated for block %d\n", m + 1);
-    }
   }
   printf("\nFile Number\tFile Size\tBlock Number\tBlock Size\tFragment");
   for (m = 0; m < number_of_files && file_arr[m] != 0; m++) {
-    externalFragmentation += fragments[m];
-    if (allocated[m])
       printf("\n%d\t\t%d\t\t%d\t\t%d\t\t%d", m, file[m], file_arr[m],
              block[file_arr[m]], fragments[m]);
   }
-  printf("\nThe external fragmentation is: %d\n\n", externalFragmentation);
 }
 
 void worstFit() {
-  int externalFragmentation;
   int fragments[10] = {0}, blocks[10], files[10];
   int m, n, number_of_blocks, number_of_files, temp, top = 0,
                                                      allocated[10] = {0};
@@ -130,24 +113,17 @@ void worstFit() {
         }
       }
       if (top) {
-        allocated[m] = 1;
         fragments[m] = top;
         block_arr[file_arr[m]] = 1;
       }
       top = 0;
-      if (!allocated[m] && n == number_of_blocks - 1) {
-        printf("No block can be allocated for block %d\n", m + 1);
-      }
     }
   }
   printf("\nFile Number\tFile Size\tBlock Number\tBlock Size\tFragment");
   for (m = 0; m < number_of_files; m++) {
-    externalFragmentation += fragments[m];
-    if (allocated[m])
       printf("\n%d\t\t%d\t\t%d\t\t%d\t\t%d", m, files[m], file_arr[m],
              blocks[file_arr[m]], fragments[m]);
   }
-  printf("\nThe external fragmentation is: %d\n\n", externalFragmentation);
 }
 
 int main() {
